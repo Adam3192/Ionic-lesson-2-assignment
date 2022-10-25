63d8bb2e55db51022e306b9b57692e6f5d28d0ef
Adam3192 <jeremyadixon@hotmail.com>
2022-10-25 15:32:03 -0500

    Pizza App

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,59 @@
 import {
   IonButton,
   IonButtons,
+  IonCheckbox,
   IonContent,
   IonHeader,
   IonIcon,
   IonInput,
   IonItem,
   IonLabel,
+  IonList,
+  IonListHeader,
   IonPage,
   IonSelect,
   IonSelectOption,
   IonTitle,
   IonToolbar,
+  useIonToast,
 } from '@ionic/react'
 import ExploreContainer from '../components/ExploreContainer'
 import './Home.css'
 
 const Home: React.FC = () => {
+  const [present] = useIonToast();
+
+  const toppings: string[] = [
+    'Pepperoni',
+    'Sausage',
+    'Extra Cheese',
+    'Bell Peppers',
+    'Onions',
+    'Mushroom',
+    'Pineapple',
+    'Black Olives',
+  ]
+
+  const displayToppings = () => {
+    return toppings.map((top) => {
+      return (
+        <IonItem key={top}>
+          <IonLabel>{top}</IonLabel>
+          <IonCheckbox></IonCheckbox>
+        </IonItem>
+      )
+    })
+  }
+
+  const orderNow = () => {
+    present({
+        color: 'dark',
+        position: 'top',
+        message: 'Order clicked',
+        duration: 2000            
+    });
+  }
+
   return (
     <IonPage>
       <IonHeader className="header">
@@ -35,7 +72,7 @@ const Home: React.FC = () => {
           <IonInput type="tel" placeholder="Enter phone number"></IonInput>
         </IonItem>
         <IonItem>
-        <IonLabel>Pizza Size</IonLabel>
+          <IonLabel>Pizza Size</IonLabel>
           <IonSelect placeholder="Select Size">
             <IonSelectOption value="Small">Small</IonSelectOption>
             <IonSelectOption value="Medium">Medium</IonSelectOption>
@@ -43,6 +80,15 @@ const Home: React.FC = () => {
             <IonSelectOption value="Extra Large">Extra Large</IonSelectOption>
           </IonSelect>
         </IonItem>
+        <br></br>
+        <br></br>
+        <IonList>
+          <IonListHeader>
+            <IonLabel>Select Toppings</IonLabel>
+          </IonListHeader>
+          {displayToppings()}
+        </IonList>
+        <IonButton onClick={ orderNow } expand="block" color="danger">Order Now</IonButton>
       </IonContent>
     </IonPage>
   )
