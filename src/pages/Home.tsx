02f5ea3d9cba47a65bce70f2d9f22f7c214973f3
Adam3192 <jeremyadixon@hotmail.com>
2022-10-25 14:34:43 -0500

    Pizza App

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,51 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonSelect,
+  IonSelectOption,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react'
+import ExploreContainer from '../components/ExploreContainer'
+import './Home.css'
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+      <IonHeader className="header">
+        <IonToolbar color="success">
+          <IonTitle>Order Details</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <h1>Enter your order details</h1>
+        <IonItem>
+          <IonLabel position="floating">Name</IonLabel>
+          <IonInput placeholder="Enter Name"></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Phone Number</IonLabel>
+          <IonInput type="tel" placeholder="Enter phone number"></IonInput>
+        </IonItem>
+        <IonItem>
+        <IonLabel>Pizza Size</IonLabel>
+          <IonSelect placeholder="Select Size">
+            <IonSelectOption value="Small">Small</IonSelectOption>
+            <IonSelectOption value="Medium">Medium</IonSelectOption>
+            <IonSelectOption value="Large">Large</IonSelectOption>
+            <IonSelectOption value="Extra Large">Extra Large</IonSelectOption>
+          </IonSelect>
+        </IonItem>
       </IonContent>
     </IonPage>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
